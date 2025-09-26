a45611be3fbb04e2a0a42e3b79dedc27a080d130
williamxieigs <yi.xie@igsl-group.com>
2025-09-26 17:46:13 +0800

    update

--- models.py
+++ models.py
@@ -1,48 +1,58 @@
 #!/usr/bin/python3
 
+from os import environ
 import torch
 from torch import nn
+from transformers import AutoConfig, Qwen3ForCausalLM
 
 class PolicyNet(nn.Module):
-  def __init__(self, action_num, hidden_dim = 8):
+  def __init__(self, action_num):
     super(PolicyNet, self).__init__()
-    self.policy = nn.Sequential(
-      nn.Conv2d(3, hidden_dim, kernel_size = (3,3), stride = (2,2), padding = 1), # (b, h, 112, 112)
-      nn.ReLU(),
-      nn.Conv2d(hidden_dim, hidden_dim, kernel_size = (3,3), stride = (2,2), padding = 1), # (b, h, 56, 56)
-      nn.ReLU(),
-      nn.Conv2d(hidden_dim, hidden_dim, kernel_size = (3,3), stride = (2,2), padding = 1), # (b, h, 28, 28)
-      nn.ReLU(),
-      nn.Conv2d(hidden_dim, hidden_dim, kernel_size = (3,3), stride = (2,2), padding = 1), # (b, h, 14, 14)
-      nn.ReLU(),
-      nn.Flatten(),
-      nn.Linear(hidden_dim * 14 * 14, hidden_dim),
-      nn.ReLU(),
-      nn.Linear(hidden_dim, action_num),
+    environ['HUGGINGFACEHUB_API_TOKEN'] = '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'
+    config = AutoConfig.from_pretrained('Qwen/Qwen3-0.6B')
+    self.encoding = nn.Sequential(
+      nn.Conv2d(3, 8, kernel_size = (3,3), stride = (2,2), padding = 1), # (b, h = 8, 112, 112)
+      nn.GELU(),
+      nn.Conv2d(8, 8, kernel_size = (3,3), stride = (2,2), padding = 1), # (b, h = 8, 56, 56)
+      nn.GELU(),
+      nn.Conv2d(8, 8, kernel_size = (3,3), stride = (2,2), padding = 1), # (b, h = 8, 28, 28)
+      nn.GELU(),
+      nn.Conv2d(8, 8, kernel_size = (3,3), stride = (2,2), padding = 1), # (b, h = 8, 14, 14)
+      nn.Flatten(), # (b, h * 14 * 14)
+      nn.Linear(8, config.hidden_size) # (b, config.hidden_size)
+    )
+    self.model = Qwen3ForCausalLM(config)
+    self.pred_head = nn.Sequential(
+      nn.Linear(config.hidden_size, action_num),
       nn.Softmax(dim = -1)
     )
-  def forward(self, x):
-    assert x.shape[1] == 3 and x.shape[2] == 224 and x.shape[3] == 224
-    weights = self.policy(x) # mean.shape = (batch, action_num)
+  def forward(self, x, past_key_values = None):
+    encoding = self.encoding(x) # encoding.shape = (b, hidden_size)
+    encoding = encoding[:,None,:] # encoding.shape = (b, 1, hidden_size)
+    results = self.model(inputs_embeds = encoding, past_key_values = past_key_values, use_cache = True)
+    hidden = results.last_hidden_state # last_hidden_state.shape = (b, 1, hidden_size)
+    past_key_values = results.past_key_values
+    hidden = torch.squeeze(hidden, dim = 1) # hidden.shape = (b, hidden_size)
+    weights = self.pred_head(hidden) # action.shape = (b, action_num)
     action = torch.multinomial(weights, 1) # action.shape = (batch, 1)
     logprob = torch.log(torch.gather(weights, dim = -1, index = action)) # logprob.shape = (batch, 1)
-    return action, logprob
+    return action, logprob, past_key_values
 
 class ValueNet(nn.Module):
   def __init__(self, hidden_dim = 8):
     super(ValueNet, self).__init__()
     self.valuenet = nn.Sequential(
       nn.Conv2d(3, hidden_dim, kernel_size = (3,3), stride = (2,2), padding = 1),
-      nn.ReLU(),
+      nn.GELU(),
       nn.Conv2d(hidden_dim, hidden_dim, kernel_size = (3,3), stride = (2,2), padding = 1),
-      nn.ReLU(),
+      nn.GELU(),
       nn.Conv2d(hidden_dim, hidden_dim, kernel_size = (3,3), stride = (2,2), padding = 1),
-      nn.ReLU(),
+      nn.GELU(),
       nn.Conv2d(hidden_dim, hidden_dim, kernel_size = (3,3), stride = (2,2), padding = 1),
-      nn.ReLU(),
+      nn.GELU(),
       nn.Flatten(),
       nn.Linear(hidden_dim * 14 * 14, hidden_dim),
-      nn.ReLU(),
+      nn.GELU(),
       nn.Linear(hidden_dim, 1)
     )
   def forward(self, x):
@@ -55,9 +65,9 @@ class PPO(nn.Module):
     self.policy_net = PolicyNet(action_num, hidden_dim)
     self.value_net = ValueNet(hidden_dim)
     self.dist = torch.distributions.Normal
-  def act(self, x):
-    action, logprob = self.policy_net(x) # action.shape = (batch, 1), logprob.shape = (batch, 1)
-    return action, logprob
+  def act(self, x, past_key_values = None):
+    action, logprob, past_key_values = self.policy_net(x, past_key_values = past_key_values) # action.shape = (batch, 1), logprob.shape = (batch, 1)
+    return action, logprob, past_key_values
   def advantages(self, states, rewards, values, dones, gamma = 0.95, lam = 0.95):
     assert states.shape[0] == rewards.shape[0] + 1 == values.shape[0] + 1 == dones.shape[0] + 1
     T = len(rewards)
